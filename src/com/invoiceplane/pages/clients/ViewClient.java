f91eb9f4f9f40cfbe7536cc6cd2795edf349bb50
aavdhootk19 <aavdhootk19@gmail.com>
2019-03-17 15:55:24 +0530

    first commit

---
+++ src/com/invoiceplane/pages/clients/ViewClient.java
@@ -0,0 +1,28 @@
+package com.invoiceplane.pages.clients;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by amolpc on 7/15/2018.
+ */
+public class ViewClient
+{
+    WebDriver driver;
+
+    WebElement btnActive;
+
+    public  ViewClient(WebDriver driver)
+    {
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public void setBtnActive()
+    {
+        btnActive.click();
+    }
+
+}
